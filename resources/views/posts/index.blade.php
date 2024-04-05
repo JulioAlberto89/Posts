3022a5c735490f42a819bfad28396ec5938d803f
JulioAlberto89 <jamv19.06.89@gmail.com>
2024-04-05 11:01:13 +0200

    Se han incorporado los posts a las vistas principales, para tener que loguearse. Se ma modificado la vista Posts y se ha incluido en el menú

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,24 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Posts') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    <form action="" method="POST">
+                        @csrf
+                        <textarea class="bg-transparent border border-gray-300 rounded-lg p-4 w-full
+                         resize-none" name="message" id="" cols="30" rows="5"
+                         placeholder="¿Qué estás pensando?"></textarea>
+                        </textarea>
+                        <x-primary-button class="mt-2">Postear</x-primary-button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -3,18 +3,21 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::view('/', 'welcome')->name('welcome');
 
 Route::middleware('auth')->group(function () {
+    Route::view('/dashboard', 'dashboard')->name('dashboard');
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/posts', function () {
+        return view('posts.index');
+    })->name('posts.index');
+
+    Route::post('/posts', function () {
+        return Request('message');
+    });
 });
 
 require __DIR__.'/auth.php';
